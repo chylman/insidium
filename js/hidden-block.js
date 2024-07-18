8daddfcf01ee82ab634e354fc22703c1ff5483cb
Ilja Afanasev <chylmanprosto@gmail.com>
2024-07-18 23:41:26 +0300

    feat: fix 2

--- js/hidden-block.js
+++ js/hidden-block.js
@@ -1,15 +1,19 @@
-const currentDate = new Date();
-const fromWednesdayToSaturday = document.querySelectorAll('[data-from-wednesday-to-saturday]');
-const fromSundayToWednesday = document.querySelectorAll('[data-from-sunday-to-wednesday]');
+setTimeout(() => {
+    const currentDate = new Date();
+    const fromWednesdayToSaturday = document.querySelectorAll('[data-from-wednesday-to-saturday]');
+    const fromSundayToWednesday = document.querySelectorAll('[data-from-sunday-to-wednesday]');
 
-if (currentDate.getDay() >= 4 && currentDate.getDay() < 7) {
-    console.log(currentDate.getDay() >= 4)
-    console.log(fromWednesdayToSaturday)
-    fromSundayToWednesday.forEach(item => {
-        item.classList.add('hidden')
-    })
-} else {
-    fromWednesdayToSaturday.forEach(item => {
-        item.classList.add('hidden')
-    })
-}
+    if (currentDate.getDay() >= 4 && currentDate.getDay() < 7) {
+        console.log(currentDate.getDay() >= 4)
+        console.log(fromWednesdayToSaturday)
+        fromSundayToWednesday.forEach(item => {
+            item.classList.add('hidden')
+        })
+    } else {
+        fromWednesdayToSaturday.forEach(item => {
+            item.classList.add('hidden')
+        })
+    }
+}, 1000)
+
+
