66abd0c24de3d7434818bd123b9086f304953107
Ilja Afanasev <chylmanprosto@gmail.com>
2024-07-18 23:31:18 +0300

    feat: fix 2

--- js/hidden-block.js
+++ js/hidden-block.js
@@ -3,11 +3,11 @@ const fromWednesdayToSaturday = document.querySelectorAll('[data-from-wednesday-
 const fromSundayToWednesday = document.querySelectorAll('[data-from-sunday-to-wednesday]');
 
 if (currentDate.getDay() >= 4 && currentDate.getDay() < 7) {
-    fromMondayToWednesday.forEach(item => {
+    fromSundayToWednesday.forEach(item => {
         item.classList.add('hidden')
     })
 } else {
-    fromWednesdayToSunday.forEach(item => {
+    fromWednesdayToSaturday.forEach(item => {
         item.classList.add('hidden')
     })
 }
