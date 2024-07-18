282904ebafaa16a88abcdbc25e6e568b85fcce7e
Ilja Afanasev <chylmanprosto@gmail.com>
2024-07-18 23:48:48 +0300

    feat: fix 2

--- js/hidden-block.js
+++ js/hidden-block.js
@@ -1,19 +1,22 @@
-setTimeout(() => {
-    const currentDate = new Date();
-    const fromWednesdayToSaturday = document.querySelectorAll('[data-from-wednesday-to-saturday]');
-    const fromSundayToWednesday = document.querySelectorAll('[data-from-sunday-to-wednesday]');
-
-    if (currentDate.getDay() >= 4 && currentDate.getDay() < 7) {
-        console.log(currentDate.getDay() >= 4)
-        console.log(fromWednesdayToSaturday)
-        fromSundayToWednesday.forEach(item => {
-            item.classList.add('hidden')
-        })
-    } else {
-        fromWednesdayToSaturday.forEach(item => {
-            item.classList.add('hidden')
-        })
-    }
-}, 1000)
+// setTimeout(() => {
+//     const currentDate = new Date();
+//     const fromWednesdayToSaturday = document.querySelectorAll('[data-from-wednesday-to-saturday]');
+//     const fromSundayToWednesday = document.querySelectorAll('[data-from-sunday-to-wednesday]');
+//     console.log(fromWednesdayToSaturday)
+//     console.log(fromSundayToWednesday)
+//
+//
+//     if (currentDate.getDay() >= 4 && currentDate.getDay() < 7) {
+//         console.log(currentDate.getDay() >= 4)
+//         console.log(fromWednesdayToSaturday)
+//         fromSundayToWednesday.forEach(item => {
+//             item.classList.add('hidden')
+//         })
+//     } else {
+//         fromWednesdayToSaturday.forEach(item => {
+//             item.classList.add('hidden')
+//         })
+//     }
+// }, 1000)
 
 
